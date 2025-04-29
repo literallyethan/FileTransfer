6c96892b153229f22fe5a06c823ecbce4a2e8d30
Ethan <ethan.k.purser@gmail.com>
2025-04-29 16:49:39 -0500

    creates Client and CLIENT_LIST, puts thought into role of server

--- backend/server/src/main.rs
+++ backend/server/src/main.rs
@@ -1,6 +1,17 @@
 use std::net::{SocketAddr, TcpListener, TcpStream};
 use std::thread;
 use std::io::Write;
+use std::sync::LazyLock;
+
+struct Client {
+    stream: TcpStream,
+    addr: SocketAddr,
+    id: u32,
+    name: String,
+}
+
+// lazylock allows list to be sync-safe (I think, never used before)
+static CLIENT_LIST: LazyLock<Vec<Client>> = LazyLock::new(|| vec![]);
 
 fn main() {
     // .expect() deals with the result instead of a pattern match
@@ -26,17 +37,31 @@ fn main() {
 }
 
 fn handle_client(mut stream: TcpStream, addr: SocketAddr) {
-    // tells client their info [temp stuff]
-
-    let addr_string: String = addr.to_string();
-    let prefix: &str = "Your info: ";
-    // the vec macro is awesome!
-    let parts: Vec<&[u8]> = vec![prefix.as_bytes(), addr_string.as_bytes()];
-    let msg_bytes: Vec<u8> = parts.concat();
+    // tells client their info
+    // in scope to free before handling loop
+    {
+        let addr_string: String = addr.to_string();
+        let prefix: &str = "Your info: ";
+        // the vec macro is awesome!
+        let parts: Vec<&[u8]> = vec![prefix.as_bytes(), addr_string.as_bytes()];
+        let msg_bytes: Vec<u8> = parts.concat();
     
-    // ignore output with _
-    let _ = stream.write_all(&msg_bytes)
-        .expect("Failed to write!");
+        // ignore output with _
+        let _ = stream.write_all(&msg_bytes)
+            .expect("Failed to write!");
 
-    println!("Sent bytes!");
+        println!("Sent bytes!");
+    }
+
+    /*
+        job of handler:
+        -accept a client
+            -place client in list of available peers
+        -provide a list of connectable peers
+        -recieve client's peer choice
+        -connect peers
+    */
+
+
+
 }
