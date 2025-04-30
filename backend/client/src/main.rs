07ec8a5fe30966e6c1225a3ce65fea0daf72f289
Ethan <ethan.k.purser@gmail.com>
2025-04-29 20:06:54 -0500

    stores connected clients in a map and sends that info to a new client on connection

--- backend/client/src/main.rs
+++ backend/client/src/main.rs
@@ -1,3 +1,4 @@
+//use core::time;
 use std::net::TcpStream;
 use std::io::Read;
 
@@ -10,10 +11,28 @@ fn main() {
     // convert that buffer to string.
     // print the string.
 
-    let mut buf: Vec<u8> = Vec::new();
-    let _ = stream.read_to_end(&mut buf)
-        .expect("Failed to read!");
+    // read() reads bytes as soon as recieved
+    let mut buf = [0u8; 1024];
+    //let _ = stream.read(&mut buf)
+    //    .expect("Failed to read!");
 
     // print the bytes as string
-    println!("{:?}", std::str::from_utf8(&buf).unwrap());
+    //println!("{:?}", std::str::from_utf8(&buf).unwrap());
+
+    loop {
+        match stream.read(&mut buf) {
+            Ok(0) => {
+                println!("Server closed connection.");
+                break;
+            }
+            Ok(n) => {
+                let text = std::str::from_utf8(&buf[..n]).unwrap_or("[Invalid UTF-8]");
+                println!("Received: {}", text);
+            }
+            Err(e) => {
+                println!("Error reading from stream: {}", e);
+                break;
+            }
+        }
+    }
 }
--- backend/server/src/main.rs
+++ backend/server/src/main.rs
@@ -1,17 +1,24 @@
 use std::net::{SocketAddr, TcpListener, TcpStream};
 use std::thread;
-use std::io::Write;
-use std::sync::LazyLock;
+use std::io::{Write, BufWriter};
+use std::sync::{LazyLock, Mutex, Arc};
+use std::collections::HashMap;
 
 struct Client {
-    stream: TcpStream,
+    stream: Arc<TcpStream>, // Arc is like a shared pointer
     addr: SocketAddr,
     id: u32,
     name: String,
 }
 
+impl Client {
+    fn new(stream: Arc<TcpStream>, addr: SocketAddr, id: u32, name: String) -> Self {
+       return Self {stream, addr, id, name};
+    }
+}
+
 // lazylock allows list to be sync-safe (I think, never used before)
-static CLIENT_LIST: LazyLock<Vec<Client>> = LazyLock::new(|| vec![]);
+static CLIENT_MAP: LazyLock<Mutex<HashMap<u32, Client>>> = LazyLock::new(|| Mutex::new(HashMap::new()));
 
 fn main() {
     // .expect() deals with the result instead of a pattern match
@@ -53,11 +60,31 @@ fn handle_client(mut stream: TcpStream, addr: SocketAddr) {
         println!("Sent bytes!");
     }
 
+    let arc_stream: Arc<TcpStream> = Arc::new(stream);
+    let id: u32 = 0;
+    let name: &str = "client0";
+    let client: Client = Client::new(arc_stream.clone(), addr, id, name.to_string());
+    let mut map = CLIENT_MAP.lock()
+        .expect("Could not lock map!");
+    
+    map.insert(client.id, client);
+
+    println!("Sending client list info: ");
+
+    let mut msg: String = String::from("List of connected clients: \n");
+    for (id, client) in map.iter() {
+        msg.push_str(&format!("Id: {}, Name: {}\n", id, &client.name));
+    }
+
+    let mut writer = BufWriter::new(arc_stream.as_ref());
+    writer.write_all(msg.as_bytes()).expect("Write failed");
+    writer.flush().expect("Flush failed");
+
     /*
         job of handler:
         -accept a client
             -place client in list of available peers
-        -provide a list of connectable peers
+        -provide a list of connectable peers (:check:)
         -recieve client's peer choice
         -connect peers
     */
